f3505aad697e1f698eabebcb94c488966e2eedd0
mn6kv <65681379+mn6kv@users.noreply.github.com>
2021-10-16 18:47:26 +0300

    Create child.py

---
+++ child.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+import os
+import time
+import random
+
+print("Child started in the procces: ", os.getpid, "with args: ", sys.argv[1])
+time.sleep(int(sys.argv[1])
+print("Child finished in the procces: ", os.getpid, "with arg: ", sys.argv[1])
+exit(random.choice[0, 1])
