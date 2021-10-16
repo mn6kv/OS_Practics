456144d4fd283df748aa38adbfff749fc852c954
mn6kv <65681379+mn6kv@users.noreply.github.com>
2021-10-16 18:37:16 +0300

    Create parent.py

---
+++ parent.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import os
+import sys
+import random
+
+flag = 1
+for i in range(n) :
+      if child == 0 :
+              break
+      child = os.fork
+if flag > 0 :
+      for i in range(0, n)
+              os.wait()
+              print("Child number ", i, "finished")
+else :
+       os.execl('child.py', "Executiom child.py", str(random.choice(range(5, 11))))
